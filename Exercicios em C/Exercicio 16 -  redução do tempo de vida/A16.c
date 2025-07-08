0bf9cc319ed1400edd0184b01ba112847ffa5129
Thayrony Von Held <thayronyvonheld@gmail.com>
2025-07-08 20:27:43 -0300

    Update A16.c

--- Exercicios em C/Exercicio 16 -  redução do tempo de vida/A16.c
+++ Exercicios em C/Exercicio 16 -  redução do tempo de vida/A16.c
@@ -3,15 +3,26 @@
 int main(){
 
 	int cigarros, anos;
-	float minutos, dias;
-	
+	float minutos_perdidos, anos_fumados;
+	float convert_vida_perdida, vida_perdida, total_fumados;
+
 	printf("Quantos cigarros voce costuma fumar por dia? ");
-		scanf("%d", cigarros);
+		scanf("%d", &cigarros);
 	
 	printf("Ha quantos anos voce fuma? ");
-		scanf("%d", anos);
+		scanf("%d", &anos);
 		
-	vida_perdida = (cigarros * 10)
-		
-}
+		anos_fumados = anos*365; 
+		total_fumados = cigarros * anos_fumados; // multiplicando o total de cigarros fumados
+			
+			vida_perdida = total_fumados * 10; // total de cigarros fumados * 10 minutos
+
+			convert_vida_perdida = vida_perdida / 1440; //convertando para dias
+
+
+	printf("Parabens, voce perdeu %.2f dias da sua vida!", convert_vida_perdida);	    
+	
+	return 0;
+	
+	}
 

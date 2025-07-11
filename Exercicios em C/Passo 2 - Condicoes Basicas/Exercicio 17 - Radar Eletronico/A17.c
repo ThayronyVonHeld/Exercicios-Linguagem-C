5832b888c4489dc21bf5aa163d619086ad127294
Thayrony Von Held <thayronyvonheld@gmail.com>
2025-07-11 19:53:12 -0300

    Exercicio 17

--- Exercicios em C/Passo 2 - Condicoes Basicas/Exercicio 17 - Radar Eletronico/A17.c
+++ Exercicios em C/Passo 2 - Condicoes Basicas/Exercicio 17 - Radar Eletronico/A17.c
@@ -2,7 +2,17 @@
 
 int main(){
 
+    int velocidade, multa;
 
+printf("Qual foi a velocidade do carro[Km/h]? ");
+    scanf("%d", &velocidade);
 
-    
+    multa = velocidade * 5;
+{
+if (velocidade > 80)
+    printf("Voce foi multado e precisa pagar R$%d", multa);
+
+    else
+        printf("Parabens, voce ficou abaixo da velocidade maxima permitida");
+}   
 }

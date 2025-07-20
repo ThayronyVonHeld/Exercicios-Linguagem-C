7c5a2973758c8bb12532bd18651030d871108165
Thayrony Von Held <thayronyvonheld@gmail.com>
2025-07-20 03:41:18 -0300

    New update

--- Exercicios em C/Passo 2 - Condicoes Basicas/Exercicio 23 - Desconto Loja/A23.c
+++ Exercicios em C/Passo 2 - Condicoes Basicas/Exercicio 23 - Desconto Loja/A23.c
@@ -1,14 +1,31 @@
 #include<stdio.h>
 
-
 int main (){
 
-    printf("Digite seu Nome");
-    scanf("%d", &nome);
+    char genero;
+    char nome[50];
+    float compras, valorFinal, desconto, F, M;
+
+    printf("Digite seu Nome: ");
+    scanf("%s", nome);
 
     printf("Digite seu genero [F] para feminino [M] para masculino: ");
-    scanf("%c", &genero);
+    scanf(" %c", &genero);
     
+    printf("Insira o valor da compra: ");
+    scanf("%f", &compras);
+   
 
-
+      if (genero == 'F' || genero == 'f') {
+        desconto = compras * 0.13;
+        valorFinal = compras - desconto;
+        printf("Parabens, %s! Voce ganhou 13%% de desconto. Total: R$%.2f\n", nome, valorFinal);
+    } else if (genero == 'M' || genero == 'm') {
+        desconto = compras * 0.05;
+        valorFinal = compras - desconto;
+        printf("Parabens, %s! Voce ganhou 5%% de desconto. Total: R$%.2f\n", nome, valorFinal);
+    } else {
+        printf("Genero invalido.\n");
+    }
+    return 0;
 }

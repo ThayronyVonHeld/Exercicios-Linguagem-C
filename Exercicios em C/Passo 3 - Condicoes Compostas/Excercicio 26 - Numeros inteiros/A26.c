80b557671b14c6703315801e6778d2b030618b16
Thayrony Von Held <thayronyvonheld@gmail.com>
2025-07-21 14:31:58 -0300

    a26

---
+++ Exercicios em C/Passo 3 - Condicoes Compostas/Excercicio 26 - Numeros inteiros/A26.c
@@ -0,0 +1,22 @@
+#include<stdio.io>
+
+main(){
+
+int n1, n2;
+    
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &n1);
+
+    printf("digite um segundo numero inteiro: ");
+    scanf("%d", &n2);
+
+    if(n1>n2){
+        printf("O primeiro valor e maior que o segundo"); 
+    }
+        else if (n2>n1) {
+    printf("O segundo valor e maior que o primeiro");
+    }
+    else 
+    printf("os valores sao iguais");
+    
+}

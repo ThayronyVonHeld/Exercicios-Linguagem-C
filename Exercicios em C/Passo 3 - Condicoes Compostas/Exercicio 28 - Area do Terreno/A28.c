aa1125eeff470788ce1d81fa4c496b09505ba813
Thayrony Von Held <thayronyvonheld@gmail.com>
2025-07-22 20:24:23 -0300

    Update A28

--- Exercicios em C/Passo 3 - Condicoes Compostas/Exercicio 28 - Area do Terreno/A28.c
+++ Exercicios em C/Passo 3 - Condicoes Compostas/Exercicio 28 - Area do Terreno/A28.c
@@ -2,4 +2,25 @@
 
 int main(){
     
+    float largura, comprimento, area;
+
+    printf("Digite a largura do terreno: ");
+    scanf("%f", &largura);
+
+    printf("Digite o comprimento do terreno: ");
+    scanf("%f", &comprimento);
+
+    area = largura * comprimento;
+
+    if (area < 100){
+    printf("Seu terreno possui %.2f m², e um terreno POPULAR", area);
+    }
+
+    else if ((area >= 100) && (area < 500)){
+        printf("Seu terreno possui %.2f m², e um terreno MASTER", area);
+    }
+    else 
+    printf("Seu terreno possui %.2f m², e um terreno VIP", area);
+
+    return 0;
 }

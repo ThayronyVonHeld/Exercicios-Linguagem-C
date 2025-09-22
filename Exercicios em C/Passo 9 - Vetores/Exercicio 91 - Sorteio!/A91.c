c52995b121efaeda40d9e4452dbd56b0c6c0f10f
Thayrony Von Held <thayronyvonheld@gmail.com>
2025-09-22 15:01:09 -0300

    Create A91

---
+++ Exercicios em C/Passo 9 - Vetores/Exercicio 91 - Sorteio!/A91.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include <stdlib.h>  // rand(), srand()
+#include <time.h>    // time()
+
+int main(){
+    
+    int vetor[7];
+    int i;
+    
+    srand(time(NULL));
+
+    for(i = 0; i < 7; i++ ){
+        vetor[i] = rand() % 100;  // números aleatórios entre 0 e 99
+    }
+
+    printf("vetor digitado:\n");
+    for(i = 0; i < 7; i++) {
+        printf("Vetor %d: %d\n", i+1, vetor[i]);
+    }
+
+    return 0;
+}

790b47b12809a5091c40cc7b8d59d50d85a2e7ac
Thayrony Von Held <thayronyvonheld@gmail.com>
2025-11-12 17:02:34 -0300

    UPDATE

--- Exercicios em C/Facu.c
+++ Exercicios em C/Facu.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-
+#include <string.h>
 
 		void opcoes(int *escolha){
 			printf("---------------------------------\n");
-			printf("Menu Lista de Dados");
+			printf("Menu Lista de Dados\n");
+			printf("---------------------------------\n");
     		printf("Digite [1] para incluir na lista\n");
    			printf("Digite [2] para consultar a lista\n");
     		printf("Digite [3] para alterar a lista\n");
@@ -12,15 +13,16 @@
 			printf("Digite sua escolha: ");
    			scanf("%d", escolha);
 		
-		return escolha;
 
 	}
 		int main(){
 			
 			char lista[10][50];
-			int i = 0;
 			int continuar;
 			int escolha;
+			int i, contagem = 0;
+			int alterar, remover;
+			
 			
 		do{
 		
@@ -28,27 +30,58 @@
 			printf("------------------------------\n");
 			
 			if(escolha == 1){
+				
 				printf("LISTA\n");
 				printf("--------------------------\n");
-            for(i; i < 50; i++){
-                printf("%d. ", i+1);
-                scanf("%s", &lista[i]);
-                printf("Digite [1] para continuar ou [0] para sair: ");
-                scanf("%d", &escolha);
-                if(escolha != 1){
-                	break;
+            
+         
+				for(i = contagem; i < 10; i++){
+               	 printf("%d. ", i + 1);
+               	 scanf("%s", lista[i]);
+               	 	contagem += 1;
+               	 	printf("Continuar Adicionando?[1]Sim [0]Nao: ");
+                	scanf("%d", &continuar);
+             
+			 	 if(continuar != 1){
                 	
+					break;
+             	
 				}
                 
 				}
-
+		
         } else if(escolha == 2){
-        	
+        	printf("Os Dados adicionados foram: \n");
+        	for(i = 0; i < contagem; i++){
+        		printf("%d. %s\n", i + 1, lista[i]);
+			}
         	
 		}
-
-    }
-    while(escolha == 0);
+		else if (escolha == 3){
+			
+			printf("Digite o numero da posicao que deseja alterar: ");
+			scanf("%d", &alterar);
+			
+			
+			printf("Agora altere o dado:\n");
+			printf("%d. ", alterar);
+			scanf("%s", lista[alterar - 1]);
+		
+			printf("Alteracao feita com Sucesso!\n");
+		}
+		else if (escolha == 4){
+			printf("Digite o numero da posicao que deseja remover: ");
+			scanf("%d", &remover);
+			
+			strcpy(lista[remover - 1], "   ");
+			
+			printf("Removido com SUCESSO!\n");
+			
+	}
+		}
+    
+	
+    while(escolha != 5);
 }
 
         
